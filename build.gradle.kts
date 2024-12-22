f0bd5af0e4396083a23eb0fa3b1d798ddb1a0f6d
Ben Forster <160018750+benfrstr@users.noreply.github.com>
2024-12-22 01:10:36 +0000

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+application.mainClass.set("com.kernsz.Apollo.bot.Main")
+group = "com.kernsz.Apollo")
+version = "1.0-SNAPSHOT"
+
+tasks.withType<ShadowJar> {
+    archiveFileName.set("Apollo.jar")
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    //Logging
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
+    //JDA
+    implementation("club.minnced:jda-ktx:0.12.0")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.isIncremental = true
+
+    //JDA supports Java 8 and above
+    sourceCompatibility = "1.8"
+}
